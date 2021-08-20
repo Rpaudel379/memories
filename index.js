c1bcdf22197c92c082b5b4f4017f1e3108292c0d
rpaudel379 <rpaudel379@gmail.com>
2021-08-20 12:09:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// mongodb
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server running on port ${PORT}`))
+  )
+  .catch((err) => console.log(err.message));
+
+mongoose.set("useFindAndModify", false);
+
+app.get("/", (req, res) => {
+  res.status(200).send("hello to memories api");
+});
+
+app.use("/posts", postRoutes);
